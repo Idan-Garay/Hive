61b85a58dc76f23974f7385158f8bb45f75e9e95
Idan Garay <47768819+Idan-Garay@users.noreply.github.com>
2024-05-07 17:50:36 +0800

    initialize CI/CD part 1

--- prettier.config.js
+++ prettier.config.js
@@ -1,6 +1,10 @@
 /** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
 const config = {
   plugins: ["prettier-plugin-tailwindcss"],
+  endOfLine: "lf",
+  printWidth: 80,
+  tabWidth: 2,
+  trailingComma: "es5",
 };
 
 export default config;
